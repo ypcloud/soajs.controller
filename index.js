0f8ec7b341e7ece029d22a474a2500a8cab8b0e0
Manuel Correa <manuelfelipe@gmail.com>
2018-03-21 18:46:03 -0400

    enable dnscache wrapper

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+var dnscache = require('dnscache')({ "enable": true, "ttl": 300, "cachesize" : 3000});
 var soajs = require('soajs');
 
 var controller = new soajs.server.controller();
